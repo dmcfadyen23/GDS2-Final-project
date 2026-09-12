d8969d6ec985ea57a6b3bca9ab481c8c85182001
Pac-ManLikeGameCreator <amitesh.d.amiteshsharma@student.uts.edu.au>
2026-09-12 16:34:41 +1000

    Lvl 3 + Respawn + Room Prog
    Forgot never pushed

---
+++ Assets/Scripts/Managers/FloorExit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class FloorExit : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset nextFloor;
+#endif
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        LoadNextFloor();
+    }
+
+    private void LoadNextFloor()
+    {
+#if UNITY_EDITOR
+        if (nextFloor == null)
+        {
+            Debug.LogError("No next floor has been assigned!");
+            return;
+        }
+
+        string scenePath = AssetDatabase.GetAssetPath(nextFloor);
+        string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+
+        Debug.Log("Loading next floor: " + sceneName);
+
+        SceneManager.LoadScene(sceneName);
+#else
+        Debug.LogError("Next floor is not configured for this build.");
+#endif
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    private void Start()
+    {
+        if (PlayerSpawn.Instance != null)
+        {
+            transform.position = PlayerSpawn.Instance.GetSpawnPosition();
+        }
+    }
+}
