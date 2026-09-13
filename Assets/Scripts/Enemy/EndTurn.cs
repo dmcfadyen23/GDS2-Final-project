35e121b758104853c2b43f9c9650ee4cc7d1bf88
dmcfadyen23 <142372378+dmcfadyen23@users.noreply.github.com>
2026-09-14 02:40:37 +1000

    temp fix ui bug

--- Assets/Scripts/Enemy/EndTurn.cs
+++ Assets/Scripts/Enemy/EndTurn.cs
@@ -28,7 +28,7 @@ public class EndTurn : MonoBehaviour
             CombatManager.WinCombat();
         }
         enemy.UseAttack(player);
-        
+        uiManager.GoToMain();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
