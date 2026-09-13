a1f2862b4b9278e9c9b2dd5a6c499493b19c0e33
Pac-ManLikeGameCreator <amitesh.d.amiteshsharma@student.uts.edu.au>
2026-09-13 12:29:31 +1000

    Attack SFX
    Attacks not really working so haven't changed the DB to add it to the individual spells

---
+++ Assets/Scripts/Attacks/AttackAudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class AttackAudioManager : MonoBehaviour
+{
+    public static AttackAudioManager Instance;
+
+    [SerializeField] private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        if (clip == null)
+            return;
+
+        if (audioSource == null)
+        {
+            Debug.LogWarning("No AudioSource");
+            return;
+        }
+
+        audioSource.PlayOneShot(clip);
+    }
+}
--- Assets/Scripts/Attacks/AttackSO.cs
+++ Assets/Scripts/Attacks/AttackSO.cs
@@ -8,6 +8,12 @@ public class AttackSO : ScriptableObject
     public string attackName = null;
     public int basePower = 0;
     public string gestureName;
+
+    [Header("Attack Sound Effects")]
+    public AudioClip drawSound;
+    public AudioClip completeSound;
+    public AudioClip hitSound;
+
     private UIManager uiManager;
 
     public enum TargetingType
@@ -39,7 +45,31 @@ public class AttackSO : ScriptableObject
         uiManager.battleLogText.text = context.sourceUnit + " used " + attackName + " and did " + damageDealt + " damage to " + context.target;
 
     }
-    
+
+    public void PlayDrawSound()
+    {
+        if (AttackAudioManager.Instance != null)
+        {
+            AttackAudioManager.Instance.PlaySound(drawSound);
+        }
+    }
+
+    public void PlayCompleteSound()
+    {
+        if (AttackAudioManager.Instance != null)
+        {
+            AttackAudioManager.Instance.PlaySound(completeSound);
+        }
+    }
+
+    public void PlayHitSound()
+    {
+        if (AttackAudioManager.Instance != null)
+        {
+            AttackAudioManager.Instance.PlaySound(hitSound);
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
